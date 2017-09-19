03553b13f88db51d28d2f191f975e5342089bc0b
cedricm <cedricm@metricstream.com>
2017-09-19 17:29:55 +0530

    This is data lesson with ngfor directive

--- my-app/src/app/servers/servers.component.ts
+++ my-app/src/app/servers/servers.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer=false;
+  serverCreated=false;
+  serverCreationStatus='No server was created';
+  serverName="Test name";
+  servers=['Test server', 'Test server 2'];
 
-  constructor() { }
+  constructor() {
+    setTimeout(()=>{
+      this.allowNewServer=true;
+    }, 2000);
+  }
 
   ngOnInit() {
   }
-
+ onCreateServer(){
+   this.servers.push(this.serverName);
+   this.serverCreationStatus='Sever was created. The server name is '+this.serverName;
+   this.serverCreated=true;
+ }
+ onUpdateServerName(event:any){
+  console.log(event);
+    this.serverName=(<HTMLInputElement>event.target).value;
+ }
 }
